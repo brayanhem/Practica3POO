3fe178a1b597038d1dd2ae747676edaf4267ab6e
Brayan Herrera <brayan.herrera@uabc.edu.mx>
2026-03-23 08:02:16 -0700

    3

--- ZombieDado.java
+++ ZombieDado.java
@@ -1,23 +1,57 @@
+import java.util.Random;
 
 public class ZombieDado
 {
     private String color;
-    private int caraActual;
+    private String caraActual;
+    private ZombieDadoView vista;
+    private Random random;
     
     
     public ZombieDado(String color){
-        
+        this.color = color;
+        this.random= new Random();
+        this.vista = new ZombieDadoView(color);
+        this.caraActual = "";
     }
     
     public void rodar(){
-        
+        int r = random.nextInt(6);
+        if (color.equals("green")) {
+            if (r <= 2) caraActual = "Cerebro";
+            else if (r <= 4) caraActual = "Pasos";
+            else caraActual = "Disparo";
+        } else if (color.equals("yellow")) {
+            if (r <= 1) caraActual = "Cerebro";
+            else if (r <= 3) caraActual = "Pasos";
+            else caraActual = "Disparo";
+        } else { // red
+            if (r == 0) caraActual = "Cerebro";
+            else if (r <= 2) caraActual = "Pasos";
+            else caraActual = "Disparo";
+        }
+        actualizarImagen();
+    }
+    
+    private void actualizarImagen(){
+        if (caraActual.equals("Cerebro")) vista.mostrarCerebro();
+        else if (caraActual.equals("Pasos")) vista.mostrarPasos();
+        else vista.mostrarDisparo();
     }
     
     public String getColor(){
-        return "";
+        return color;
     }
     
-    public int getCaraActual(){
-        return 0;
+    public String getCaraActual(){
+        return caraActual;
+    }
+    
+    public void mover(int x, int y){
+        vista.moverDados(x,y);
+    }
+    
+    public void ocultar(){
+        vista.hacerInvisible();
     }
 }
